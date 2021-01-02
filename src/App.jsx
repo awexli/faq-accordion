31e152598c61d33c8c2f2c1c808bc67d8358ac38
Alex Li <sf.alexli@gmail.com>
2021-01-01 20:07:43 -0800

    complete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -24,32 +24,74 @@ const MainContainer = styled.div`
     var(--gradient-violet) 35%,
     var(--gradient-blue) 100%
   );
+
+  // Expands background gradient on small viewports
   height: 100vh;
+  overflow: auto;
+
   display: flex;
   justify-content: center;
   align-items: center;
+  color: var(--color-text-primary-dark);
 `;
 
 const MainCard = styled.div`
   background: white;
   width: 300px;
-  height: 70%;
   overflow: hidden;
   border-radius: calc(16px * 1.5);
   box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+
+  @media (min-width: 576px) {
+    width: 400px;
+  }
+
+  @media (min-width: 768px) {
+    width: 500px;
+  }
 `;
 
 const CardWrapper = styled.div`
   padding: 1rem;
+
+  h1 {
+    text-align: center;
+    margin-top: 0;
+  }
 `;
 
-const messages = {
-  i: {
+const Attribution = styled.div`
+  font-size: 11px;
+  text-align: center;
+`;
+
+const messages = [
+  {
+    question: 'How many team members can I invite?',
+    answer:
+      'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.',
+  },
+  {
     question: 'What is the maximum file upload size?',
     answer:
       'No more than 2GB. All files in your account must fit your allotted storage space',
   },
-};
+  {
+    question: 'How do I reset my password?',
+    answer:
+      'Click “Forgot password” from the login page or “Change password” from your profile page. \n\nA reset link will be emailed to you.',
+  },
+  {
+    question: 'Can I cancel my subscription?',
+    answer:
+      'Yes! Send us a message and we’ll process your request no questions asked.',
+  },
+  {
+    question: 'Do you provide additional support?',
+    answer:
+      'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
+  },
+];
 
 export const App = () => {
   return (
@@ -59,7 +101,13 @@ export const App = () => {
         <MainCard>
           <CardWrapper>
             <h1>FAQ</h1>
-            <Accordion faq={messages.i}/>
+            {messages.map((message, ndx) => (
+              <Accordion key={ndx} faq={message} />
+            ))}
+            <Attribution>
+              Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>
+              . Coded by <a href="https://github.com/awexli">Alex Li</a>
+            </Attribution>
           </CardWrapper>
         </MainCard>
       </MainContainer>
--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import { ArrowDown } from '../assets';
 
 const AccordionContainer = styled.div`
+  margin-bottom: 8px;
+  border-bottom: 1px solid var(--divider-grey);
+
+  // Important for animation
+  max-height: 45px;
+  overflow: hidden;
+  transition: max-height ease-out 0.2s;
+
   * {
     font-size: var(--font-size-body);
+
+    @media (min-width: 576px) {
+      font-size: 16px;
+    }
   }
 `;
 
@@ -12,24 +25,84 @@ const AccordionHeader = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  cursor: pointer;
+
+  transition: color ease 0.1s;
+  
+  :hover {
+    color: var(--color-text-primary-red);
+  }
 `;
 
 const AccordionHeading = styled.h2`
-  margin: 0;
+  margin: 8px 0;
 `;
 
 const AccordionMessage = styled.p`
-  color: var(--color-text-neutral-grey);
+  color: var(--color-text-neutral-dark);
   line-height: 1.5;
-  height: 40px;
+  margin: 8px 0;
+  white-space: pre-line;
 `;
 
+const ArrowButton = styled.button`
+  cursor: pointer;
+  border: none;
+  background: transparent;
+
+  :focus {
+    outline: none;
+  }
+`;
+
+const getAccordionContainer = (numOfChildren, currentTarget) => {
+  let parent = currentTarget;
+
+  for (let i = 0; i < numOfChildren; ++i) {
+    parent = parent.parentNode;
+  }
+
+  return parent;
+}
+
 export const Accordion = ({ faq }) => {
+  const [expand, setExpand] = useState(false);
+
+  const handleArrowClick = (e) => {
+    const target = e.target;
+    let panel;
+
+    if (target.nodeName === "path") {
+      panel = getAccordionContainer(4, target);
+    } else if (target.nodeName === 'svg') {
+      panel = getAccordionContainer(3, target);
+    } else if (target.nodeName === 'H2' || target.nodeName === 'BUTTON') {
+      panel = getAccordionContainer(2, target);
+    } else {
+      panel = target.parentNode;
+    }
+
+    if (!panel) {
+      setExpand(!expand);
+      return;
+    }
+    
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 'px';
+    }
+
+    setExpand(!expand);
+  };
+
   return (
-    <AccordionContainer>
-      <AccordionHeader>
+    <AccordionContainer isExpand={expand}>
+      <AccordionHeader onClick={handleArrowClick}>
         <AccordionHeading>{faq.question}</AccordionHeading>
-        <ArrowDown />
+        <ArrowButton>
+          <ArrowDown isExpand={expand} />
+        </ArrowButton>
       </AccordionHeader>
       <AccordionMessage>{faq.answer}</AccordionMessage>
     </AccordionContainer>
